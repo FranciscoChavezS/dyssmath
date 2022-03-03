7fb81774c149d011550b5aba1628b41bf9bf0263
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-03-02 21:38:33 -0600

    Filtrar Cursos por categorías y niveles

--- app/Http/Livewire/CourseIndex.php
+++ app/Http/Livewire/CourseIndex.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 use App\Models\Course;
+use App\Models\Level;
+use Livewire\WithPagination;
 
 class CourseIndex extends Component
 {
+    use WithPagination; //Paginación dinámica
+
+    public $category_id;
+    public $level_id;
+
     public function render()
     {
-       
-        $courses=Course::where('status',3)->latest('id')->paginate(8);
-        
-        return view('livewire.course-index', compact('courses'));
+        $categories = Category::all();
+        $levels = Level::all();
+
+
+        //Mostramos los cursos con status 3 (PUBLICADO) y ordenamos los cursos por ID
+        $courses = Course::where('status', 3)
+                        ->category($this->category_id) //Filtrar cursos por categorías y niveles con método Scope en el modelo
+                        ->level($this->level_id)
+                        ->latest('id')
+                        ->paginate(8);
+
+
+        return view('livewire.course-index', compact('courses', 'categories','levels'));
+    }
+    //Reiniciar filtrado de cursos 
+    public function resetFilters(){
+        $this->reset(['category_id','level_id']);
     }
 }
--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="bg-gray-200 py-4 mb-16">
+     <div class="bg-gray-200 py-4 mb-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">
         <button class="bg-white shadow h-12 px-4 rounded-lg mr-4 text-gray-700 focus:outline-none"wire:click="resetFilters">
             <i class="fas fa-book-reader text-xs mr-2"></i>
@@ -16,9 +16,9 @@
                 </button>
                 <!-- Dropdown Body -->
                 <div class="absolute right-0 w-40 mt-2 py-2 bg-white border rounded shadow-xl"x-show="open"x-on:click.away="open=false">   
-                    {{-- @foreach($categories as $category) --}}
-                    <a class=" cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white" {{-- wire:click="$set('category_id',{{$category->id}})" --}} x-on:click="open=false">{{-- {{$category->name}} --}}</a>
-                    {{-- @endforeach --}}
+                    @foreach($categories as $category) 
+                        <a class=" cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white"wire:click="$set('category_id',{{$category->id}})" x-on:click="open=false">{{$category->name}}</a>
+                     @endforeach
                      
                     <div class="py-2">
                   
@@ -36,9 +36,9 @@
                 </button>
                 <!-- Dropdown Body -->
                 <div class="absolute right-0 w-40 mt-2 py-2 bg-white border rounded shadow-xl"x-show="open"x-on:click.away="open=false">   
-                    {{-- @foreach($levels as $level) --}}
-                    <a class=" cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white"{{-- wire:click="$set('level_id',{{$level->id}})" --}} x-on:click="open=false">{{-- {{$level->name}} --}}</a>
-                    {{-- @endforeach --}}
+                    @foreach($levels as $level)
+                        <a class=" cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white"wire:click="$set('level_id',{{$level->id}})" x-on:click="open=false">{{$level->name}}</a>
+                    @endforeach
                      
                     <div class="py-2">
                   
@@ -57,48 +57,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-x-6 gap-y-8">
         @foreach($courses as $course)
 
-            <article class="bg-white shadow-lg rounded overflow-hidden">
-                <img  class="h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
-
-                <div class="px-6 py-4">
-                    <h1 class="text-xl text-gray-700 mb-2 leading-6">
-                        {{Str::limit($course->title,40)}}
-                    </h1>
-                    <p class="text-gray-500 text-sm mb-2">Prof: {{$course->teacher->name}}</p>
-
-                    <div class="flex">
-                        <ul class="flex text-sm">
-                            <li class="mr-1 ">
-                                <i class="fas fa-star text-{{$course->rating >= 1 ? 'yellow' : 'gray'}}-400"></i>
-                            </li>
-                            <li class="mr-1 ">
-                                <i class="fas fa-star text-{{$course->rating >= 2 ? 'yellow' : 'gray'}}-400"></i>
-                            </li>
-                            <li class="mr-1 ">
-                                <i class="fas fa-star text-{{$course->rating >= 3 ? 'yellow' : 'gray'}}-400"></i>
-                            </li>
-                            <li class="mr-1 ">
-                                <i class="fas fa-star text-{{$course->rating >= 4 ? 'yellow' : 'gray'}}-400"></i>
-                            </li>
-                            <li class="mr-1 ">
-                                <i class="fas fa-star text-{{$course->rating == 5 ? 'yellow' : 'gray'}}-400"></i>
-                            </li>
-                        </ul>
-
-                        <p class="text-sm text-gray-500 ml-auto">
-                            <i class="fas fa-users"></i>
-                            ({{$course->students_count}})
-                        </p>
-
-
-                    </div>
-
-                    <a href="{{route('courses.show', $course)}}" class="block text-center w-full mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        ENTRAR A CURSO
-                    </a>
-                    
-                </div>
-            </article>
+        <x-course-card :course="$course"/>
 
         @endforeach
     </div>
@@ -106,5 +65,5 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 mb-8">
         {{$courses->links()}}
     </div>
-    
+   
 </div>
