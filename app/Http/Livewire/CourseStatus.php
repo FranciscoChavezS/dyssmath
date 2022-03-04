4a4c34f8eb6f989c3ddcbc8c0e92ca2dc4c95741
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-03-03 20:39:33 -0600

    Botones para Anterior y Siguiente

--- app/Http/Livewire/CourseStatus.php
+++ app/Http/Livewire/CourseStatus.php
@@ -3,12 +3,13 @@
 namespace App\Http\Livewire;
 
 use App\Models\Course;
+use App\Models\Lesson;
 use Livewire\Component;
 
 class CourseStatus extends Component
 {
-    public $course;
-    public $current;
+    public $course, $current;
+
 
     public function mount(Course $course){
         $this->course = $course;
@@ -16,6 +17,8 @@ class CourseStatus extends Component
         foreach($course->lessons as $lesson){
             if(!$lesson->completed){
                 $this->current = $lesson;
+
+
                 break; 
             }
         }
@@ -24,5 +27,37 @@ class CourseStatus extends Component
     public function render()
     {
         return view('livewire.course-status');
+    }
+
+    //metodos
+    public function changeLesson(Lesson $lesson){
+        $this->current=$lesson;
+
+    }
+
+    //Propiedades computadas
+    public function getIndexProperty()
+    {
+        return $this->course->lessons->pluck('id')->search($this->current->id);
+    }
+    
+    public function getPreviousProperty()
+    {
+        if($this->index==0){
+            return null;
+        }else{
+             return $this->course->lessons[$this->index - 1];
+        }
+    }
+
+    public function getNextProperty()
+    {
+        if($this->index==$this->course->lessons->count()-1){
+
+            return null;
+           }else{
+    
+            return $this->course->lessons[$this->index + 1];
+           }
     }
 }
