d9471024558e3ecda7524d3bba2ae656d649e016
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-03-03 19:36:51 -0600

    Barra de buscador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,8 @@
                     Es aplicación web interactiva destinada a niños de 6 a 9 años con discalculia, para poder complementar el aprendizaje del individuo en el ámbito del razonamiento matemático.
                 </p>
 
-                
-                <!-- BUSCADOR -->
-                    <div class="pt-2 relative mx-auto text-gray-600">
-                        <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                        type="search" name="search" placeholder="Search">
-
-                        <button type="submit" class="absolute right-0 top-0 mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Buscar
-                        </button>
-                    </div>
+                @livewire('search')
+               
             </div>
             
         </div>
